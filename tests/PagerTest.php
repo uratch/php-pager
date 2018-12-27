6ce38eec586d829a01ccf0ccfb1216f978f85334
uratch <kenji.uraoka@gmail.com>
2018-12-27 15:49:43 +0900

    何故かdataProviderでの実装が構造上出来ないと以前思って断念したんだけど、今見たら簡単に出来たので修正

--- tests/PagerTest.php
+++ tests/PagerTest.php
@@ -17,42 +17,43 @@ class PagerTest extends PHPUnit_Framework_TestCase
         $this->Pager = new Pager();
     }
 
-    public function testGet()
+    /**
+     * @dataProvider dataProvider
+     */
+    public function testGet($total, $page, $cur, $min, $max)
     {
-        /* 
-         * set test array 
-         *
-         * [ total, page, cur, min, max ]
-         */
-        $arr[] = array(20387, 1, 1, 1, 5);
-        $arr[] = array(20387, 2, 2, 1, 5);
-        $arr[] = array(20387, 3, 3, 1, 5);
-        $arr[] = array(20387, 4, 4, 2, 6);
-        $arr[] = array(20387, 5, 5, 3, 7);
-        $arr[] = array(50, 6, 1, 1, 5);
-        $arr[] = array(51, 6, 6, 2, 6);
-        $arr[] = array(51, 5, 5, 2, 6);
-        $arr[] = array(51, 4, 4, 2, 6);
-        $arr[] = array(51, 3, 3, 1, 5);
-        $arr[] = array(51, 2, 2, 1, 5);
-        $arr[] = array(51, 1, 1, 1, 5);
-        $arr[] = array(0,  6, 1, 1, 1);
-        $arr[] = array(0,  -1, 1, 1, 1);
-        $arr[] = array(20, -2, 1, 1, 2);
-        $arr[] = array(20, 1, 1, 1, 2);
-        $arr[] = array(20, 2, 2, 1, 2);
-        $arr[] = array(20, 3, 1, 1, 2);
-        $arr[] = array(3,  2, 1, 1, 1);
-        $arr[] = array(3,  0, 1, 1, 1);
-        $arr[] = array(3,  1, 1, 1, 1);
-        $arr[] = array(3, -1, 1, 1, 1);
-        $arr[] = array(-1, 1, 1, 1, 1);
+        $pager = $this->Pager->Get($total, $page);
+        $this->assertEquals($cur, $pager['cur']);
+        $this->assertEquals($min, $pager['min']);
+        $this->assertEquals($max, $pager['max']);
+    }
 
-        foreach ($arr as $k => $v) {
-            $pager = $this->Pager->Get($arr[$k][0], $arr[$k][1]);
-            $this->assertEquals($arr[$k][2], $pager['cur']);
-            $this->assertEquals($arr[$k][3], $pager['min']);
-            $this->assertEquals($arr[$k][4], $pager['max']);
-        }
+    public function dataProvider()
+    {
+        return [
+          [20387, 1, 1, 1, 5],
+          [20387, 2, 2, 1, 5],
+          [20387, 3, 3, 1, 5],
+          [20387, 4, 4, 2, 6],
+          [20387, 5, 5, 3, 7],
+          [50, 6, 1, 1, 5],
+          [51, 6, 6, 2, 6],
+          [51, 5, 5, 2, 6],
+          [51, 4, 4, 2, 6],
+          [51, 3, 3, 1, 5],
+          [51, 2, 2, 1, 5],
+          [51, 1, 1, 1, 5],
+          [0,  6, 1, 1, 1],
+          [0,  -1, 1, 1, 1],
+          [20, -2, 1, 1, 2],
+          [20, 1, 1, 1, 2],
+          [20, 2, 2, 1, 2],
+          [20, 3, 1, 1, 2],
+          [3,  2, 1, 1, 1],
+          [3,  0, 1, 1, 1],
+          [3,  1, 1, 1, 1],
+          [3, -1, 1, 1, 1],
+          [-1, 1, 1, 1, 1],
+        ];
     }
 }
